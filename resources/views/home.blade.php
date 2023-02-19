6917234bfc4c1d6a1a6c3c211fc27094ca18fcb3
Magda <ma.wernik@gmail.com>
2023-02-19 17:20:50 +0100

    slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,13 +27,53 @@
 
                     {{ __('Estás logead@!') }}
                 </div>
-            </div>
+            </div> 
         </div>
     </div>
 </div>
+<div id="explanation-container">
+    <div class="explanation"> ¡Que nada te pare! Aquí tienes frases con palabras de apoyo y de ánimo de lo más especiales, que te motivarán y te inspirarán siempre que lo necesites en tu vida. ¡Guárdalas dando click a la estrella para poder recordarlas luego!</div>
+</div>
+{{-- SLIDER  --}}
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="storage\app\public\images\landscape1.jpg" class="d-block w-50" alt="landscape">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="storage\app\public\images\landscape2.jpg" class="d-block w-50" alt="landscape">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="storage\app\public\images\landscape3.jpg" class="d-block w-50" alt="landscape">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 @endsection
-
-
 
 </body>
 </html>
