e8713da8d1a6aaff9058b00a21a83d5784302e23
pohengchan <pohengchan@gmail.com>
2023-02-20 14:31:53 +0100

    add slider

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Phrase;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $phrases = Phrase::where('is_onslider', 1)->get();
+       
+        return view('home', compact('phrases'));
+        //return view('home');
     }
+
+
 }
--- app/Http/Controllers/PhraseController.php
+++ app/Http/Controllers/PhraseController.php
@@ -16,12 +16,30 @@ class PhraseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+         /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
     public function index()
     {
-        $phrases = Phrase::paginate();
+         //$phrases = Phrase::paginate();
+        $phrases = Phrase::where('is_onslider', 1)->get();
 
-        return view('phrase.index', compact('phrases'))
-            ->with('i', (request()->input('page', 1) - 1) * $phrases->perPage());
+        return view('phrase.index', compact('phrases'));
+          //  ->with('i', (request()->input('page', 1) - 1) * $phrases->perPage());
     }
 
     /**
